09b8dba745de8d0d290bc03cefbaa434a7f4e59e
/Coder_404 <mikeykkj15@gmail.com>
2025-07-26 15:52:45 +0100

    Cors habilitation

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-from fastapi import FastAPI 
-from pydantic import BaseModel 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import google.generativeai as gemini
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class Pergunta(BaseModel):
-    prompt : str
-    
+    prompt: str
+
 gemini.configure(api_key='AIzaSyBSvCe6OXLOMlz8pOwpK4R-q8YAP9piZeY')
+
 @app.post("/bot/")
 async def chatbot(pergunta: Pergunta):
     modelo = gemini.GenerativeModel("gemini-1.5-flash")
     response = modelo.generate_content(pergunta.prompt)
-    
+
     return {"resposta": response.text}
